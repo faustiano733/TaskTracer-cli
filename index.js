68730fa6a13d9a53a577fe797a5e71ade56e86e5
faustiano733 <faustiangerald18@gmail.com>
2026-06-08 14:25:23 +0100

    Projecto completo

--- index.js
+++ index.js
@@ -12,42 +12,85 @@ const arg1 = args[0].toLowerCase()
 await createDb()
 const taskRepository = new TaskRepository
 await taskRepository.init()
+let taskId = undefined
+let tasks = []
 switch(arg1){
     case 'add':
-        const taskName = args[1]
-        if(!taskName){
+        const taskDescription = args[1].toLowerCase()
+
+        if(!taskDescription){
             console.log('Inclua o nome da tarefa')
         }
-
-        taskRepository.add(taskName)
+        taskRepository.add(taskDescription)
         break;
     case 'delete':
         console.log('Deletando tarefa')
-        break;
+        taskId = args[1]
+        if(!taskId){
+            console.log('insira o id da tarefa')
+            break
+        }
+        taskRepository.delete(taskId)
+        break
     case 'mark-in-progress':
-        console.log('Tarefa em progresso')
+        taskId = args[1]
+        if(!taskId){
+            console.log('insira o id da tarefa')
+            break
+        }
+
+        taskRepository.markInProgress(taskId)
         break;
     case 'mark-done':
-        console.log('Tarefa Concluida')
+        taskId = args[1]
+        if(!taskId){
+            console.log('insira o id da tarefa')
+            break
+        }
+        await taskRepository.markDone(taskId)
         break;
     case 'list':
         const arg2 = args[1]
         switch(arg2){
             case 'done':
-                console.log('tarefas concluidas:')
+                tasks = await taskRepository.findByStatus(arg2) || []
+                if(!(tasks.length > 0)){
+                    console.log("Não existem tarefas")
+                    break
+                }
+
+                console.log(tasks)
                 break;
             case 'in-progress':
-                console.log('tarefas em progresso:')
+                tasks = await taskRepository.findByStatus(arg2) || []
+                if(!(tasks.length > 0)){
+                    console.log("Não existem tarefas")
+                    break
+                }
+
+                console.log(tasks)
                 break;
             case 'todo':
-                console.log('tarefas incompletas:')
+                tasks = await taskRepository.findByStatus(arg2) || []
+                if(!(tasks.length > 0)){
+                    console.log("Não existem tarefas")
+                    break;
+                }
+
+                console.log(tasks)
                 break;
             case undefined:
-                console.log('todas as tarefas')
+                tasks = await taskRepository.findAll() || []
+           
+                if(!(tasks.length > 0)){
+                    console.log("Não existem tarefas")
+                    break;
+                }
+
+                console.log(tasks)
                 break;
             default:
                 console.log('Argumento invalido')
-            
         }
         break
     default:
--- repository/taskRepository.js
+++ repository/taskRepository.js
@@ -16,10 +16,12 @@ export class TaskRepository {
     async commit(){
         await fs.writeFile(this.dbPath, JSON.stringify(this.db))
     }
+
     async add(description){
         const now = new Date
         const task = {
             id : this.db.last_id + 1,
+            description,
             createdAt : now,
             updatedAt : now,
             status : 'todo',
@@ -31,5 +33,75 @@ export class TaskRepository {
 
         return task
         
+    }
+
+    async delete(id){
+        const taskIndex = this.binaryTaskSearch(id)
+        
+        if(taskIndex == -1){
+            console.log('Tarefa nao existe')
+            return false
+        }
+        this.db.task.splice(taskIndex, 1)
+        await this.commit()
+        return true
+        
+    }
+
+    async markInProgress(id){
+        const taskIndex = this.binaryTaskSearch(id)
+        if(taskIndex == -1){
+            console.log('Tarefa nao existe')
+            return false
+        }
+
+        this.db.task[taskIndex].status = "in-progress";
+        await this.commit();
+        return true
+    }
+    async markDone(id){
+        const taskIndex = this.binaryTaskSearch(id)
+        if(taskIndex == -1){
+            console.log('Tarefa nao existe')
+            return false
+        }
+
+        this.db.task[taskIndex].status = "done";
+        await this.commit();
+        return true
+    }
+    
+    async findAll(){
+        return this.db.task
+    }
+    async findByStatus(status){
+        let filteredTasks = this.db.task.filter(task=>task.status == status)
+
+        return filteredTasks
+    }
+
+    binaryTaskSearch(id){
+        const tasks = this.db.task
+        let max = tasks.length - 1
+        let min = 0
+
+        while(min <= max){
+            const middle = Math.floor((min + max) / 2);
+            const task = tasks[middle]
+            const taskId = task?.id 
+            if( taskId == id){
+                return middle
+            }else{
+            if(taskId > id){
+                max = middle - 1
+            }
+
+              if(taskId < id){
+                min = middle + 1
+              }
+            }
+
+        }
+        return -1    
     }
 }
